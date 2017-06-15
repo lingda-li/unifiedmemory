1faf65a6da6966313709e31391efb48ace01779d
Lingda Li <lildmh@gmail.com>
2017-06-15 12:39:47 -0400

    update test

--- code/minitest/test.cu
+++ code/minitest/test.cu
@@ -16,24 +16,33 @@ int main()
     int size = SIZE;
     int BlockSize = 256;
     int BlockNum = (size + BlockSize - 1) / BlockSize;
-    int *a, *b;
+    int *a, *b; 
     int *d_a, *d_b;
+    int sum_a, sum_b;
+    sum_a = sum_b = 0;
 
     a = (int*)malloc(size*sizeof(int));
     b = (int*)malloc(size*sizeof(int));
-    for(i = 0; i < size; i++)
+    for(i = 0; i < size; i++) {
         a[i] = rand() % 100;
+        sum_a += a[i];
+    }
 
-	cudaMalloc((void **)&d_a, size*sizeof(int));
-	cudaMemcpy(d_a, a, size*sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_b, b, size*sizeof(int), cudaMemcpyHostToDevice);
-	cudaMalloc((void **)&d_b, size*sizeof(int));
+    cudaMalloc((void **)&d_a, size*sizeof(int));
+    cudaMemcpy(d_a, a, size*sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, size*sizeof(int), cudaMemcpyHostToDevice);
+    cudaMalloc((void **)&d_b, size*sizeof(int));
 
     MyKernel<<<BlockNum, BlockSize>>>(d_a, d_b, size);
 
-	cudaMemcpy(b, d_b, size*sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(b, d_b, size*sizeof(int), cudaMemcpyDeviceToHost);
     cudaFree(d_a);
     cudaFree(d_b);
+
+    for(i = 0; i < size; i++)
+        sum_b += b[i];
+
+    printf("sum_a: %d, sum_b: %d\n", sum_a, sum_b);
 
     return 0;
 }
