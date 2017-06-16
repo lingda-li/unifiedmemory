fbc81e66e6afb554179398e1dec2a0f8452d0f10
Lingda Li <lildmh@gmail.com>
2017-06-16 11:07:31 -0400

    update mini test

--- code/minitest/test_m.cu
+++ code/minitest/test_m.cu
@@ -17,16 +17,27 @@ int main()
     int BlockSize = 256;
     int BlockNum = (size + BlockSize - 1) / BlockSize;
     int *d_a, *d_b;
+    int sum_a, sum_b;
+    sum_a = sum_b = 0;
 
 	cudaMallocManaged((void **)&d_a, size*sizeof(int));
 	cudaMallocManaged((void **)&d_b, size*sizeof(int));
-    for(i = 0; i < size; i++)
+    for(i = 0; i < size; i++) {
         d_a[i] = rand() % 100;
+        sum_a += d_a[i];
+    }
 
     MyKernel<<<BlockNum, BlockSize>>>(d_a, d_b, size);
 
+    cudaDeviceSynchronize();
+
+    for(i = 0; i < size; i++)
+        sum_b += d_b[i];
+
     cudaFree(d_a);
     cudaFree(d_b);
+
+    printf("sum_a: %d, sum_b: %d\n", sum_a, sum_b);
 
     return 0;
 }
