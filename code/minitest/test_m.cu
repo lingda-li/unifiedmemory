1f995a33bc20036daf56beed6a9c9d4863b8a719
Lingda Li <lildmh@gmail.com>
2017-06-27 17:45:02 -0400

    add debug info in runtime

--- code/minitest/test_m.cu
+++ code/minitest/test_m.cu
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MYDEBUG
+#ifdef MYDEBUG
+#define DEBUG_PRINT printf("here: %d\n", __LINE__); fflush(stdout);
+#else
+#define DEBUG_PRINT
+#endif
+
 #define SIZE 10240
 
 __global__ void MyKernel(int *a, int *b, int size)
@@ -19,23 +26,29 @@ int main()
     int *d_a, *d_b;
     int sum_a, sum_b;
     sum_a = sum_b = 0;
+    DEBUG_PRINT
 
 	cudaMallocManaged((void **)&d_a, size*sizeof(int));
 	cudaMallocManaged((void **)&d_b, size*sizeof(int));
+    DEBUG_PRINT
     for(i = 0; i < size; i++) {
         d_a[i] = rand() % 100;
         sum_a += d_a[i];
     }
+    DEBUG_PRINT
 
     MyKernel<<<BlockNum, BlockSize>>>(d_a, d_b, size);
 
     cudaDeviceSynchronize();
+    DEBUG_PRINT
 
     for(i = 0; i < size; i++)
         sum_b += d_b[i];
 
+    DEBUG_PRINT
     cudaFree(d_a);
     cudaFree(d_b);
+    DEBUG_PRINT
 
     printf("sum_a: %d, sum_b: %d\n", sum_a, sum_b);
 
--- code/uvm-runtime/uvm-runtime.cu
+++ code/uvm-runtime/uvm-runtime.cu
@@ -1,7 +1,15 @@
+#include "stdio.h"
 #include "uvm-runtime.h"
 
 #define ALL_MANAGED
 #define GPU_PRE_PASCAL
+
+#define UVM_DEBUG
+#ifdef UVM_DEBUG
+#define DEBUG_PRINT printf("here: %d\n", __LINE__); fflush(stdout);
+#else
+#define DEBUG_PRINT
+#endif
 
 void __uvm_malloc(struct uvmMallocInfo* uvmInfo)
 {
@@ -16,6 +24,9 @@ void __uvm_malloc(struct uvmMallocInfo* uvmInfo)
   cudaMalloc(&uvmInfo->devPtr, size);
   uvmInfo->isSame = false;
 #endif
+#ifdef UVM_DEBUG
+  printf("Debug: __uvm_malloc 0x%lx 0x%lx (%lu)\n", (unsigned long)uvmInfo->hostPtr, (unsigned long)uvmInfo->devPtr, size); fflush(stdout);
+#endif
 }
 
 void __uvm_free(struct uvmMallocInfo* uvmInfo)
@@ -23,6 +34,9 @@ void __uvm_free(struct uvmMallocInfo* uvmInfo)
   cudaFree(uvmInfo->devPtr);
   if (!uvmInfo->isSame)
     free(uvmInfo->hostPtr);
+#ifdef UVM_DEBUG
+  printf("Debug: __uvm_free 0x%lx 0x%lx\n", (unsigned long)uvmInfo->hostPtr, (unsigned long)uvmInfo->devPtr); fflush(stdout);
+#endif
 }
 
 void __uvm_memcpy(struct uvmMallocInfo* uvmInfo, cudaMemcpyKind kind)
@@ -42,4 +56,7 @@ void __uvm_memcpy(struct uvmMallocInfo* uvmInfo, cudaMemcpyKind kind)
     cudaMemcpy(devPtr, hostPtr, size, kind);
   else
     cudaMemcpy(hostPtr, devPtr, size, kind);
+#ifdef UVM_DEBUG
+  printf("Debug: __uvm_memcpy 0x%lx 0x%lx\n", (unsigned long)uvmInfo->hostPtr, (unsigned long)uvmInfo->devPtr); fflush(stdout);
+#endif
 }
