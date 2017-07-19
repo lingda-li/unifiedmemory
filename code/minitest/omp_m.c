4892ff3f2435f8c399d24b4c8fe00f11098b421e
Lingda Li <lildmh@gmail.com>
2017-07-19 14:28:33 -0400

    modify omp uvm test with omp_target_alloc

--- code/minitest/omp_m.c
+++ code/minitest/omp_m.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <cuda_runtime_api.h>
 #include <cuda.h>
+#include <omp.h>
 
 #define MYDEBUG
 #ifdef MYDEBUG
@@ -10,13 +11,17 @@
 #define DEBUG_PRINT
 #endif
 
-//#define USE_UVM
+#define OMP_ALLOC
+//#define HYB_ALLOC
 //#define UVM_ALLOC
 //#define HOST_ALLOC
 //#define DEVICE_ALLOC
 
-#define SIZE 10240
-//#define SIZE (8 * 1024 * 1024 / sizeof(int) * 1024)
+//#define NO_HOST_ACCESS
+#define SEC_CALL
+
+//#define SIZE 10240
+#define SIZE (18 / 3 * 1024 * 1024 / sizeof(double) * 1024)
 
 int main()
 {
@@ -27,7 +32,15 @@ int main()
     sum_a = sum_c = 0.0;
     DEBUG_PRINT
 
-#if defined(UVM_ALLOC)
+#if defined(OMP_ALLOC)
+    d_a = (double *)omp_target_alloc(size * sizeof(double), omp_get_default_device());
+    d_b = (double *)omp_target_alloc(size * sizeof(double), omp_get_default_device());
+    d_c = (double *)omp_target_alloc(size * sizeof(double), omp_get_default_device());
+#elif defined(HYB_ALLOC)
+    d_a = (double *)malloc(size*sizeof(double));
+    d_b = (double *)malloc(size*sizeof(double));
+    d_c = (double *)omp_target_alloc(size * sizeof(double), omp_get_default_device());
+#elif defined(UVM_ALLOC)
 	cudaMallocManaged((void **)&d_a, size*sizeof(double), cudaMemAttachGlobal);
 	cudaMallocManaged((void **)&d_b, size*sizeof(double), cudaMemAttachGlobal);
 	cudaMallocManaged((void **)&d_c, size*sizeof(double), cudaMemAttachGlobal);
@@ -40,46 +53,79 @@ int main()
 	cudaMalloc((void **)&d_b, size*sizeof(double));
 	cudaMalloc((void **)&d_c, size*sizeof(double));
 #else
-    d_a = (double*)malloc(size*sizeof(double));
-    d_b = (double*)malloc(size*sizeof(double));
-    d_c = (double*)malloc(size*sizeof(double));
+    d_a = (double *)malloc(size*sizeof(double));
+    d_b = (double *)malloc(size*sizeof(double));
+    d_c = (double *)malloc(size*sizeof(double));
 #endif
     DEBUG_PRINT
+#ifndef NO_HOST_ACCESS
     for(i = 0; i < size; i++) {
         d_a[i] = (rand() % 10) * 0.5;
         d_b[i] = (rand() % 2 + 1) * 1.0;
         sum_a += d_a[i];
     }
     DEBUG_PRINT
+#endif
 
-#ifdef USE_UVM
+#if defined(OMP_ALLOC)
 #pragma omp target data map(to:size)
+#elif defined(HYB_ALLOC)
+#pragma omp target data map(to:size) map(to:d_a[0:size]) map(to:d_b[0:size])
 #else
-#pragma omp target data map(to:d_a[0:size]) map(to:d_b[0:size]) map(from:d_c[0:size])
+#pragma omp target data map(to:size) map(to:d_a[0:size]) map(to:d_b[0:size]) map(from:d_c[0:size])
 #endif
     {
-#ifdef USE_UVM
+#if defined(OMP_ALLOC)
 #pragma omp target teams distribute parallel for is_device_ptr(d_a) is_device_ptr(d_b) is_device_ptr(d_c)
+#elif defined(HYB_ALLOC)
+#pragma omp target teams distribute parallel for is_device_ptr(d_c)
 #else
 #pragma omp target teams distribute parallel for
 #endif
     for (uint64_t i = 0; i < size; i++) {
         d_c[i] = d_a[i] / d_b[i];
     }
+#ifdef SEC_CALL
+    cudaDeviceSynchronize();
+#if defined(OMP_ALLOC)
+#pragma omp target teams distribute parallel for is_device_ptr(d_a) is_device_ptr(d_b) is_device_ptr(d_c)
+#elif defined(HYB_ALLOC)
+#pragma omp target teams distribute parallel for is_device_ptr(d_c)
+#else
+#pragma omp target teams distribute parallel for
+#endif
+    for (uint64_t i = 0; i < size; i++) {
+        d_c[i] += d_a[i] / d_b[i];
+    }
+#endif
     }
     cudaDeviceSynchronize();
 
     DEBUG_PRINT
 
+#ifndef NO_HOST_ACCESS
     for(i = 0; i < size; i++)
         sum_c += d_c[i];
+#endif
 
     printf("sum_a: %f, sum_c: %f\n", sum_a, sum_c);
 
-#if defined(UVM_ALLOC) || defined(HOST_ALLOC) || defined(DEVICE_ALLOC)
+#if defined(OMP_ALLOC)
+    omp_target_free(d_a, omp_get_default_device());
+    omp_target_free(d_b, omp_get_default_device());
+    omp_target_free(d_c, omp_get_default_device());
+#elif defined(HYB_ALLOC)
+    free(d_a);
+    free(d_b);
+    omp_target_free(d_c, omp_get_default_device());
+#elif defined(UVM_ALLOC) || defined(HOST_ALLOC) || defined(DEVICE_ALLOC)
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
+#else
+    free(d_a);
+    free(d_b);
+    free(d_c);
 #endif
     return 0;
 }
