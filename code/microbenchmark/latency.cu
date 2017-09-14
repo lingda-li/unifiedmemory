8c9552deb459b85804699ca4b727a1864c3249f8
Lingda Li <lildmh@gmail.com>
2017-09-14 17:06:19 -0400

    update throughput test

--- code/microbenchmark/latency.cu
+++ code/microbenchmark/latency.cu
@@ -1,30 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define DEVICE_ALLOC
-//#define UVM_ALLOC
+//#define DEVICE_ALLOC
+#define UVM_ALLOC
 //#define HOST_ALLOC
 
-//#define SIZE (2048 * 4)
+//#define SIZE (1024 * 8)
 //#define STEP 512
 
 //#define SIZE (1024 * 1024)
 //#define STEP (1024 * 32)
+//#define STEP 512
 
-//#define SIZE (1024 * 1024 * 1024)
-//#define STEP (1024 * 1024 * 32)
-
-#define SIZE (1024 * 1024 * 1024L * 2)
+#define SIZE (1024 * 1024 * 1024)
 #define STEP (1024 * 1024 * 32)
+
+//#define SIZE (1024 * 1024 * 1024L * 2)
+//#define STEP (1024 * 1024 * 32)
 
 //#define PRINT_LAT
 
-__global__ void kernel(int *input, unsigned *total_lat)
+__global__ void kernel(int *input, double *total_lat)
 {
   unsigned t0, t1, lat;
   __shared__ int s_tmp;
   int tmp;
+  unsigned maxlat, minlat;
 
   s_tmp = 0;
+  maxlat = minlat = 0;
   for (unsigned long long i = 0; i < SIZE; i += STEP) {
     t0 = clock();
     __syncthreads();
@@ -33,17 +37,23 @@ __global__ void kernel(int *input, unsigned *total_lat)
     t1 = clock();
     lat = t1 - t0;
 #ifdef PRINT_LAT
-    printf("%llu: %d\n", i, lat);
+    printf("0x%10llx: %d\n", i, lat);
 #endif
     s_tmp += lat;
+    if (lat > maxlat)
+      maxlat = lat;
+    if (lat < minlat || minlat == 0)
+      minlat = lat;
   }
-  *total_lat = s_tmp;
+  total_lat[0] = s_tmp;
+  total_lat[1] = maxlat;
+  total_lat[2] = minlat;
 }
 
 int main()
 {
   int *d_input;
-  unsigned *total_lat, h_total_lat;
+  double *total_lat, h_total_lat[3];
 
 #if defined(DEVICE_ALLOC)
   cudaMalloc(&d_input, SIZE*sizeof(int));
@@ -54,14 +64,30 @@ int main()
 #else
   return 0;
 #endif
-  cudaMalloc(&total_lat, sizeof(unsigned));
+  cudaMalloc(&total_lat, 3*sizeof(double));
+
+  // init
+#if defined(DEVICE_ALLOC)
+  int *h_input;
+  h_input = (int*)malloc(SIZE*sizeof(int));
+  for (unsigned long long i = 0; i < SIZE; i += STEP) {
+    h_input[i] = rand();
+  }
+  cudaMemcpy(d_input, h_input, SIZE*sizeof(int), cudaMemcpyHostToDevice);
+#elif defined(UVM_ALLOC) || defined(HOST_ALLOC)
+  for (unsigned long long i = 0; i < SIZE; i += STEP) {
+    d_input[i] = rand();
+  }
+#endif
 
   kernel<<<1, 1>>>(d_input, total_lat);
 
-  cudaMemcpy(&h_total_lat, total_lat, sizeof(unsigned), cudaMemcpyDeviceToHost);
+  cudaMemcpy(&h_total_lat[0], total_lat, 3*sizeof(double), cudaMemcpyDeviceToHost);
   cudaFree(d_input);
   cudaFree(total_lat);
-  double AvgLat = (double)h_total_lat / (SIZE / STEP);
+  double AvgLat = h_total_lat[0] / (SIZE / STEP);
   printf("Average latency: %f\n", AvgLat);
+  printf("Max latency: %f\n", h_total_lat[1]);
+  printf("Min latency: %f\n", h_total_lat[2]);
   return 0;
 }
---
+++ code/microbenchmark/throughput_rw.cu
@@ -0,0 +1,100 @@
+#include <stdio.h>
+
+//#define DEVICE_ALLOC
+#define UVM_ALLOC
+//#define HOST_ALLOC
+
+//#define SIZE (2048 * 4)
+//#define SIZE (1024 * 1024)
+//#define SIZE (1024 * 1024 * 1024)
+#define SIZE (1024 * 1024 * 1024L * 2)
+
+__global__ void kernel(int *input, unsigned long long size)
+{
+  unsigned i = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (i < size)
+    input[i] += 1;
+}
+
+int main()
+{
+  int *d_input;
+
+  cudaEvent_t start;
+  cudaEvent_t end;
+
+#if defined(DEVICE_ALLOC)
+  cudaMalloc(&d_input, SIZE*sizeof(int));
+#elif defined(UVM_ALLOC)
+  cudaMallocManaged(&d_input, SIZE*sizeof(int));
+#elif defined(HOST_ALLOC)
+  cudaMallocHost(&d_input, SIZE*sizeof(int));
+#else
+  return 0;
+#endif
+
+  // init
+#if defined(DEVICE_ALLOC)
+  int *h_input;
+  h_input = (int*)malloc(SIZE*sizeof(int));
+  for (unsigned long long i = 0; i < SIZE; i++) {
+    h_input[i] = rand() % 10;
+  }
+  cudaMemcpy(d_input, h_input, SIZE*sizeof(int), cudaMemcpyHostToDevice);
+#elif defined(UVM_ALLOC) || defined(HOST_ALLOC)
+  for (unsigned long long i = 0; i < SIZE; i++) {
+    d_input[i] = rand() % 10;
+  }
+#endif
+
+  unsigned ThreadNum = 256;
+  unsigned long long BlockNum = (SIZE - 1) / ThreadNum + 1;
+
+  cudaEventCreate(&start);
+  cudaEventCreate(&end);
+  cudaEventRecord(start,0);
+
+  kernel<<<BlockNum, ThreadNum>>>(d_input, SIZE);
+
+  cudaThreadSynchronize();
+  cudaEventRecord(end, 0);
+  cudaEventSynchronize(end);
+  float elapsed_time1;
+  cudaEventElapsedTime(&elapsed_time1, start, end);
+
+  cudaEventCreate(&start);
+  cudaEventCreate(&end);
+  cudaEventRecord(start,0);
+
+  kernel<<<BlockNum, ThreadNum>>>(d_input, SIZE);
+
+  cudaThreadSynchronize();
+  cudaEventRecord(end, 0);
+  cudaEventSynchronize(end);
+  float elapsed_time2;
+  cudaEventElapsedTime(&elapsed_time2, start, end);
+
+  cudaEventCreate(&start);
+  cudaEventCreate(&end);
+  cudaEventRecord(start,0);
+
+  kernel<<<BlockNum, ThreadNum>>>(d_input, SIZE);
+
+  cudaThreadSynchronize();
+  cudaEventRecord(end, 0);
+  cudaEventSynchronize(end);
+  float elapsed_time3;
+  cudaEventElapsedTime(&elapsed_time3, start, end);
+
+  double AvgTP1 = (double)SIZE*sizeof(int) / (elapsed_time1 / 1000.0) / 1e9;
+  double AvgTP2 = (double)SIZE*sizeof(int) / (elapsed_time2 / 1000.0) / 1e9;
+  double AvgTP3 = (double)SIZE*sizeof(int) / (elapsed_time3 / 1000.0) / 1e9;
+  printf("Average throughput: %f GB/s, %f GB/s, %f GB/s\n", AvgTP1, AvgTP2, AvgTP3);
+  printf("Time: %f ms, %f ms, %f ms\n", elapsed_time1, elapsed_time2, elapsed_time3);
+
+  cudaFree(d_input);
+  cudaEventDestroy(start);
+  cudaEventDestroy(end);
+  return 0;
+}
